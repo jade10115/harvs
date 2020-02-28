4d457b21327cd1b2a1b4f7a2f5b600c7b4bd84a6
Franz <foibanez@gmail.com>
2020-02-28 15:06:22 -0800

    minor changes on placeholder

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -9,11 +9,11 @@
 			    <?=form_open('auth/validate'); ?>
 					  <div class="form-group">
 					    <label for="exampleInputEmail1">Email address</label>
-					    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+					    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="username">
 					  </div>
 					  <div class="form-group">
 					    <label for="exampleInputPassword1">Password</label>
-					    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+					    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
 					  </div>
 					  <div class="form-check">
 					    <input type="checkbox" class="form-check-input" id="exampleCheck1">
