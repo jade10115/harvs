a7bfdb132120ff15e9eb81fce11008f9fbe87d75
jade10115 <jadebrosas143@gmail.com>
2020-02-28 13:19:57 +0800

    update

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -17,4 +17,41 @@ class Auth extends CI_Controller {
 	public function logout(){
 		header('location:'.base_url('/'));
 	}
+	public function validate()
+	{
+	  $username = $this->input->post('username');
+      $password = $this->input->post('password');
+      $this->db->select('*');
+      $this->db->from('tbl_user');
+      $this->db->where('username',$username);
+      $this->db->where('password',$password);
+  		  
+      $query = $this->db->get();
+      $result = $query->result_array();
+     
+      	# code...
+      
+      if($query->num_rows() > 0)
+      {
+            $this->session->set_userdata('user_id',$result);
+
+            if($query->row('user_type_id') == 5){
+                redirect('Admin');
+            }elseif ($query->row('user_level') == 'student') {
+                redirect('event_user');
+         } elseif ($query->row('user_level') == 'stud_pres') {
+                redirect('event_pres');
+            }
+            else{
+                redirect('login');
+            }
+
+      }else{
+            echo "<p style='color: red;font-weight: bold;'>Wrong username or password! </p>";
+      }
+  
+}
+ 
+ 
+	
 }
