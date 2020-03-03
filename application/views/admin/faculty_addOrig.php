6ee780e83570ef403c6be9ea95484d70e4b057ad
mr-use <traehdlocaj@gmail.com>
2020-03-03 16:29:01 +0800

    formlayout

--- application/views/admin/faculty_addOrig.php
+++ application/views/admin/faculty_addOrig.php
@@ -16,74 +16,43 @@
 			  <div class="card-body faculty">
           <?=form_open_multipart('admin/addFaculty', 'id="frm_faculty_add"');?>
           <!--row start-->
+
           <div class="row">
-              <div class="col-md-7 mt-4">
-                  <div class="row">
-                      <div class="col-md-6">
-                          <div class="form-group fixlabel">
-                              <input style="background: none" type="text" class="form-control" name="identification" id="identification" required readonly>
-                              <label>Faculty ID</label>
-                          </div>
-                      </div>
-                      <div class="col-md-6">
-                          <div class="form-group">
-                              <select class="custom-select" name="department_id" required>
-                                  <option value=""></option>
-                                  <?php foreach ($user_types as $row) {?>
-                                      <option value="<?=$row['user_type_id']?>"><?=$row['user_type']?></option>
-                                  <?php } ?>
-                              </select>
-                              <label>Usertype</label>
-                          </div>
-                      </div>
-                  </div>    
-                  <div class="row">
-                      <div class="col-md-12">
-                          <div class="form-group">
-                              <input type="text" class="form-control" name="username" required>
-                              <label>Username</label>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="row">
-                      <div class="col-md-12">
-                          <div class="form-group">
-                              <input type="text" class="form-control" name="password" required>
-                              <label>Password</label>
-                          </div>
-                      </div>
-                  </div>    
-              </div> 
-              <div class="col-md-5">
-                  <div class="row d-flex justify-content-center">
-                      <div class="col-md-9">
-                          <label>Faculty Picture <small>(optional)</small></label>
-                          <div class="row">
-                              <div class="card profile col-md-10 p-0" style="height: 200px;">
-                                  <img src="<?=base_url('assets/img/users/avatar.png')?>" height="100%" width="100%" class="hand imgClick">
-                                  <input type="file" id="imgFile" class="invisible position-absolute" name="image_src">
-                              </div>
-                              <div class="col-md-1">
-                                  <a href="#" tabindex="-1" class="btn btn-sm btn-outline-primary mt-1 mb-1 imgClick" rel="tooltip" data-toggle="tooltip" data-placement="left" title="Select Picture">
-                                    <span data-feather="image"></span>
-                                  </a>
-                                  <a href="#" tabindex="-1" class="btn btn-sm btn-outline-success mt-1 mb-1" rel="tooltip" data-toggle="tooltip" data-placement="left" title="Reset Picture">
-                                      <span data-feather="refresh-ccw"></span>
-                                  </a>
-                                  <a href="#" tabindex="-1" class="btn btn-sm btn-outline-danger mt-1 mb-1" rel="tooltip" data-toggle="tooltip" data-placement="left" title="Remove Picture" id="removePicture">
-                                      <span data-feather="x"></span>
-                                  </a>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div> 
+            <div class="col-md-2">
+              <div class="form-group fixlabel">
+                <input style="background: none" type="text" class="form-control" name="identification" id="identification" required readonly>
+                <label>Faculty ID</label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-group">
+                <input type="text" class="form-control" name="username" required>
+                <label>Username</label>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-group">
+                <input type="text" class="form-control" name="password" required>
+                <label>Password</label>
+              </div>
+            </div>
+            <div class="col-md-2">
+              <div class="form-group">
+                <select class="custom-select" name="department_id" required>
+                  <option value=""></option>
+                  <?php foreach ($user_types as $row) {?>
+                  <option value="<?=$row['user_type_id']?>"><?=$row['user_type']?></option>
+                  <?php } ?>
+                </select>
+                <label>Usertype</label>
+              </div>
+            </div>
           </div>
           <hr>
-
           <div class="row">
 
-            <div class="col-md-12">
+            <div class="col-md-8">
+
               <div class="row">
                 <div class="col-md-4">
                   <div class="form-group">
@@ -133,14 +102,14 @@
 
               <!--row start-->
               <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-6">
                   <div class="form-group">
                     <input type="text" class="form-control" name="email" required>
                     <label>Email</label>
                   </div>
                 </div>
 
-                <div class="col-md-4">
+                <div class="col-md-6">
                   <div class="form-group">
                     <input type="text" class="form-control" name="contact_no" required>
                     <label>Contact Number</label>
@@ -151,7 +120,20 @@
             </div>
 
             <!--faculty picture-->
-            
+            <div class="col-md-4">
+               <label>Faculty Picture <small>(optional)</small></label>
+               <div class="row">
+                 <div class="card profile col-md-10 p-0" style="height: 200px;">
+                   <img src="<?=base_url('assets/img/users/avatar.png')?>" height="100%" width="100%" class="hand imgClick">
+                   <input type="file" id="imgFile" class="invisible position-absolute" name="image_src">
+                 </div>
+                 <div class="col-md-1">
+                   <a href="#" tabindex="-1" class="btn btn-sm btn-outline-primary mt-1 mb-1 imgClick" rel="tooltip" data-toggle="tooltip" data-placement="left" title="Select Picture"><span data-feather="image"></span></a>
+                   <a href="#" tabindex="-1" class="btn btn-sm btn-outline-success mt-1 mb-1" rel="tooltip" data-toggle="tooltip" data-placement="left" title="Reset Picture"><span data-feather="refresh-ccw"></span></a>
+                   <a href="#" tabindex="-1" class="btn btn-sm btn-outline-danger mt-1 mb-1" rel="tooltip" data-toggle="tooltip" data-placement="left" title="Remove Picture" id="removePicture"><span data-feather="x"></span></a>
+                 </div>
+               </div>
+            </div>
           </div>
           <!--row end-->
 
