7cb4e14a8b389b320987e00e7a3fdf553a326b02
06rey <junrey.algario@gmail.com>
2020-03-05 16:29:01 +0800

    minor changes

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -9,11 +9,11 @@
 			    <?=form_open('auth/validate'); ?>
 					  <div class="form-group">
 					    <label for="exampleInputEmail1">Email address</label>
-					    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="username">
+					    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="username" value="admin">
 					  </div>
 					  <div class="form-group">
 					    <label for="exampleInputPassword1">Password</label>
-					    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
+					    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password" value="admin">
 					  </div>
 					  <div class="form-check">
 					    <input type="checkbox" class="form-check-input" id="exampleCheck1">
