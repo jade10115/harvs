0c14e5787653a5c38f76c19d3f13077b337ca66e
lodimasipag <foibanez@gmail.com>
2020-03-12 16:20:55 +0800

    test commit

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -23,6 +23,15 @@
 		$(document).ready(function(){
 			base_url = '<?=base_url()?>';
 
+
+			$(document).bind("DOMNodeInserted", function() {
+			   $(this).find('.dt-buttons').addClass('btn-group');
+			   $(this).find('.buttons-print').addClass('btn btn-sm btn-outline-primary');
+			   $(this).find('.buttons-excel').addClass('btn btn-sm btn-outline-danger');
+			   $(this).find('.buttons-csv').addClass('btn btn-sm btn-outline-info');
+			   $(this).find('.buttons-pdf').addClass('btn btn-sm btn-outline-success');
+			});
+
 			$('.formEdit').attr('disabled', true);
 
 			$('.selectpicker').selectpicker();
